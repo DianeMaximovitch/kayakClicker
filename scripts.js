0b84dff26ed9855ef4984a78d10473c29da08cf0
Diane Maximovitch <diane.maximovitch284@gmail.com>
2022-01-21 10:58:41 +0100

    kayak clicker - première beta

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var html_kayak = document.querySelector("#gamezone img");
+var html_kayak_counter = document.querySelector("#kayak");
+var html_boost = document.querySelector("#boost");
+var html_boost_price = document.querySelector("#booster-price");
+
+var nbKayak = 0;
+var boostPrice = 10;
+var kayakPerClick = 1;
+
+html_kayak.addEventListener("click", function() {
+  nbKayak = nbKayak + kayakPerClick;
+  html_kayak_counter.textContent = nbKayak;
+});
+
+html_boost.addEventListener("click", function() {
+  if (nbKayak >= boostPrice) {
+    nbKayak = nbKayak - boostPrice;
+    html_Kayak_counter.textContent = nbKayak;
+    KayakPerClick++;
+    boostPrice = boostPrice * 10;
+    html_boost_price.textContent = boostPrice;
+  } else {
+    alert("Pas assez de kayak ! Déso");
+  }
+});
